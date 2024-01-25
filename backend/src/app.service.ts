fe225f87917e7d54e237ef17209a07ea5242a6e0
Ryu WSL <ry-tanaka@sios.com>
2024-01-25 21:56:10 +0900

    check env

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   constructor() {}
   async getHello(): Promise<string> {
-    return `Hello World!`;
+    return `Hello World!!!!!!`;
   }
 }
