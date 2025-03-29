dd52cae3eaced05a27d25fae48c1ae0bc7833b2b
ryu_wsl <ry-tanaka@sios.com>
2025-03-29 06:44:49 +0000

    feature chenge

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/')
+  @Get('/api')
   async getHello(): Promise<string> {
     console.log(`request`);
     return 'come on';
