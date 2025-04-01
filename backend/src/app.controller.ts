646c995b3a829872232d6b50047e44fddd62608e
ryu_wsl <ry-tanaka@sios.com>
2025-04-01 01:11:00 +0000

    feature:remove /api/auth

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,19 +1,9 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { IsGoogleAuthenticatedGuard } from './common/guard/is-google-authenticated/is-google-authenticated.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get('/api/auth')
-  @UseGuards(IsGoogleAuthenticatedGuard)
-  async getHello(
-    @Body() req: { googleAccessToken: string; googleRefreshToken: string },
-  ): Promise<{ googleAccessToken: string; googleRefreshToken: string }> {
-    return { googleAccessToken: req.googleAccessToken, googleRefreshToken: req.googleRefreshToken };
-    // return this.appService.getHello();
-  }
 
   @Post('/api/assignRoles')
   async postHello(@Body() req: { userDetails: string }): Promise<{ roles: string[] }> {
--- backend/src/common/guard/is-google-authenticated/is-google-authenticated.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { IsGoogleAuthenticatedGuard } from './is-google-authenticated.guard';
-
-describe('IsGoogleAuthenticatedGuard', () => {
-  it('should be defined', () => {
-    expect(new IsGoogleAuthenticatedGuard()).toBeDefined();
-  });
-});
